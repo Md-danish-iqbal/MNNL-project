04cd1726f919d188d3926142b8fe98231ac442f6
danish iqbal <diqbal9804@gmail.com>
2020-11-15 13:56:38 +0530

    initial copmit

---
+++ test.py
@@ -0,0 +1,14 @@
+<html>
+<head>
+  <title>Title of the document</title>
+</head>
+
+<body>
+  <h1>This is a heading</h1>
+  <p>This is a paragraph.</p>
+
+
+  <script src="script.js"> </script>
+</body>
+
+</html>
